af1b4aadd8d87c989a81fb0479d46e7d09727f38
const <const@KETCH>
2017-02-15 12:06:30 +0300

    embedded fonts

--- BVPN_DOOR/bvpndoor/bvpn.cpp
+++ BVPN_DOOR/bvpndoor/bvpn.cpp
@@ -6,4 +6,41 @@ CBlinkVPN::CBlinkVPN(QWidget* parent)
     : QWidget(parent)
 {
     ui.setupUi(this);
+
+    setupFonts();
+
+}
+
+bool CBlinkVPN::setupFonts(void) Q_DECL_NOEXCEPT
+{
+    int id(0);
+
+    id = getEmbeddedFontId("Roboto Bold");
+    setupEmbeddedFont("loginLabel", id, 9);
+    setupEmbeddedFont("connectButton", id, 11);
+
+    return true;
+}
+
+long CBlinkVPN::getEmbeddedFontId(const QString& alias) Q_DECL_NOEXCEPT
+{
+    return QFontDatabase::addApplicationFont(":/CBlinkVPN/" + alias);
+}
+
+bool CBlinkVPN::setupEmbeddedFont(const QString& wname, int id, int fsize, bool bold) Q_DECL_NOEXCEPT
+{
+    QWidget* w = findChild<QWidget*>(wname);
+    if (w && (id != -1))
+    {
+        QFont font;
+
+        font.setFamily(QFontDatabase::applicationFontFamilies(id).at(0));
+        font.setPointSize(fsize);
+        font.setStyleStrategy(QFont::PreferAntialias);
+        font.setBold(bold);
+
+        w->setFont(font);
+        return true;
+    }
+    return false;
 }
--- BVPN_DOOR/bvpndoor/bvpn.h
+++ BVPN_DOOR/bvpndoor/bvpn.h
@@ -10,6 +10,12 @@ class CBlinkVPN : public QWidget
 public:
     CBlinkVPN(QWidget* parent = Q_NULLPTR);
 
+protected:
+    // helper methods
+    bool setupFonts(void) Q_DECL_NOEXCEPT;
+    long getEmbeddedFontId(const QString&) Q_DECL_NOEXCEPT;
+    bool setupEmbeddedFont(const QString&, int, int, bool = false) Q_DECL_NOEXCEPT;
+
 private:
     Ui::BlinkVPNClass ui;
 };
